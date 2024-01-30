e074a67e643c55df6b3911b23afac79766efc8bc
viktor <vkt.knnnk@gmail.com>
2024-01-31 03:39:57 +0500

    added RelayCommand for writing MVVM commands, added GetRequest which makes GET request to remote website, added Apartment - this our future DB table

---
+++ PriceService/Models/Apartment.cs
@@ -0,0 +1,11 @@
+﻿namespace PriceService.Models
+{
+    public class Apartment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float? PriceMortgageMonthly { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ PriceService/Models/GetRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PriceService.Models
+{
+    class GetRequest
+    {
+        private readonly HttpClient _client;
+        private readonly string _url;
+        public GetRequest(string url)
+        {
+            _client = new HttpClient();
+            _url = url;
+        }
+        public async Task<string> RunRequest()
+        {
+            using (HttpResponseMessage response = await _client.GetAsync(_url))
+            {
+                if (response is not null && response.IsSuccessStatusCode)
+                {
+                    var source = await response.Content.ReadAsStringAsync();
+                    _client.Dispose();
+                    return source;
+                }
+                else
+                    throw new ArgumentNullException("empty response");
+            }
+            throw new InvalidOperationException("bad url");
+        }
+    }
+}
