1e9f86b4f5ca11dc2ef0676a2b4175287fb9549f
viktor <vkt.knnnk@gmail.com>
2024-01-31 20:40:23 +0500

    added DB-Context, connected DB

--- PriceService/Models/Apartment.cs
+++ PriceService/Models/Apartment.cs
@@ -1,11 +1,16 @@
-﻿namespace PriceService.Models
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PriceService.Models
 {
-    public class Apartment
+    public partial class Apartment
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public float Price { get; set; }
-        public float? PriceMortgageMonthly { get; set; }
+        public double Price { get; set; }
+        public double? PriceMortgageMonthly { get; set; }
         public string Url { get; set; }
     }
 }
---
+++ PriceService/Models/PrinzipDBContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PriceService.Models
+{
+    public partial class PrinzipDBContext : DbContext
+    {
+        public PrinzipDBContext()
+        {
+        }
+
+        public PrinzipDBContext(DbContextOptions<PrinzipDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Apartment> Apartments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-CBSCN3M; database=PrinzipDB; integrated security = true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Apartment>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(70);
+
+                entity.Property(e => e.Url)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
